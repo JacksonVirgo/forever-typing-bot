ca86f4065e8930e1577b9e62ba999a515f18eb3a
JacksonVirgo <jacknash42@gmail.com>
2025-02-11 12:45:44 +1000

    lol

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import { Client, GatewayIntentBits, TextChannel } from "discord.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+});
+
+client.once("ready", () => {
+  console.log(`Logged in as ${client.user?.tag}`);
+  startTyping();
+});
+
+async function startTyping() {
+  const channel = await client.channels.fetch(Deno.env.get("CHANNEL_ID")!);
+  if (!channel || !channel.isTextBased())
+    return console.error("Invalid channel");
+
+  while (true) {
+    (channel as TextChannel).sendTyping();
+    await new Promise((resolve) => setTimeout(resolve, 5000)); // 5s delay
+  }
+}
+
+client.login(Deno.env.get("DISCORD_TOKEN")!);
